4b1505774ed790b8837f3ad7c17d31910882878d
Jim Hemming <jim.hemming@gmail.com>
2020-04-08 14:14:09 +0100

    initial windows forms

---
+++ Classes/Companies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Companies
+{
+	public Companies()
+	{
+
+        
+	}
+
+    public string companyName { get; set; }
+
+    public int companyID { get; set; }
+
+    public string companyAddr { get; set; }
+
+    public string companyPhone { get; set; }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MADAM-Control
 MSP Automated Documentation and Monitoring Control Application
+
+Central control application to connect to server applications
