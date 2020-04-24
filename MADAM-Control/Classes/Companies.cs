d8492e8dc04c1948f3ae34965fa8287016844600
Jim Hemming <jim.hemming@gmail.com>
2020-04-24 13:31:05 +0100

    add companies class

---
+++ MADAM-Control/Classes/Companies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace MADAM_Control.Classes
+{
+    class Companies
+    {
+        public string CompName { get; set; }
+        public string CompAddrLine1 { get; set; }
+        public string CompAddrLine2 { get; set; }
+        public string CompAddrLine3 { get; set; }
+        public string CompPostcode { get; set; }
+        public string CompTelNo { get; set; }
+        public IPAddress CompServerIp { get; set; }
+        public string CompContactName { get; set; }
+        public string CompContactEmail { get; set; }
+            
+    }
+}
